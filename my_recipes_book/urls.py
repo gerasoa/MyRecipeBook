22ebae516338ed0baf27e271e6e45214dd6de923
Rogerio Soares <gerasoa@gmail.com>
2025-01-24 22:58:10 +0000

    Fixed errors

--- my_recipes_book/urls.py
+++ my_recipes_book/urls.py
@@ -22,5 +22,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('my_app.urls'), name='home'),
+    path('', include('recipes.urls'), name='home'),
 ]
