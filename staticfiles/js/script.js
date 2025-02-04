d6e2c686c718fc06abc4ee0bdae8a2cfcebd5660
Rogerio Soares <gerasoa@gmail.com>
2025-02-04 22:13:01 +0000

    Add nonce attribute to scripts for CSP compliance; update templates and styles for improved layout and accessibility

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,1 +1,46 @@
-console.log("Life, The Universe and Everything!");
+document.addEventListener('DOMContentLoaded', function () {
+    const stars = document.querySelectorAll('.star-rating .fa-star');
+    const ratingValue = document.querySelector('.rating-value');
+
+    stars.forEach(star => {
+      star.addEventListener('click', function () {
+        const rating = this.getAttribute('data-rating');
+        ratingValue.value = rating;
+
+        stars.forEach(s => {
+          s.classList.remove('checked');
+        });
+
+        for (let i = 0; i < rating; i++) {
+          stars[i].classList.add('checked');
+        }
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.favorite-icon').forEach(function (icon) {
+      icon.addEventListener('click', function () {
+        var recipeId = this.getAttribute('data-recipe-id');
+        var iconElement = this;
+        fetch(`/recipes/toggle_favorite/${recipeId}/`, {
+          method: 'POST',
+          headers: {
+            'X-CSRFToken': '{{ csrf_token }}',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({})
+        })
+          .then(response => response.json())
+          .then(data => {
+            if (data.is_favorite) {
+              iconElement.classList.remove('far');
+              iconElement.classList.add('fas');
+            } else {
+              iconElement.classList.remove('fas');
+              iconElement.classList.add('far');
+            }
+          });
+      });
+    });
+  });
