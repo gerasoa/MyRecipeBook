9c16108898bdc17412a0a957bd3dc9f0d9a8b80d
Rogerio Soares <gerasoa@gmail.com>
2025-02-04 08:17:53 +0000

    Enable SSL redirection and configure proxy settings for secure connections

--- my_recipes_book/settings.py
+++ my_recipes_book/settings.py
@@ -164,3 +164,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
