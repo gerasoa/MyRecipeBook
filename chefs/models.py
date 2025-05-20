47b0afeccba770c74c194099fad9987ce94f2faa
Rogerio Soares <gerasoa@gmail.com>
2025-05-20 23:21:08 +0100

    feat: configured image upload to use Cloudinary instead of local media storage

--- chefs/models.py
+++ chefs/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 class ChefProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='chef_profile')
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='chef_profile'
+    )
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     bio = models.TextField()
-    # photo_small = models.ImageField(upload_to='chefs/small/', null=True, blank=True)
     photo_small = CloudinaryField('photo_small', blank=True, null=True)
-    # photo_large = models.ImageField(upload_to='chefs/large/', null=True, blank=True)
     photo_large = CloudinaryField('photo_large', blank=True, null=True)
     updated_on = models.DateTimeField(auto_now=True)
 
