1f0f640ac201061bf8b9c427d057e7b7667c5775
Rogerio Soares <gerasoa@gmail.com>
2025-05-20 19:44:48 +0100

    fix: refactor allowed_hosts

--- my_recipes_book/settings.py
+++ my_recipes_book/settings.py
@@ -33,8 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
  
-ALLOWED_HOSTS = ['.herokuapp.com',
-                 '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 
                         'https://*.herokuapp.com']
